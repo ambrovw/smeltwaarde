b299fdc64347a7578ea206e78913a821fca97151
Ambro <ambro@ambrovanwyk.com>
2025-09-25 10:58:23 +0200

    Localhost sign in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,7 +37,7 @@ export default function Login({ setIsLoggedIn }) {
             <GoogleLogin
                 onSuccess={handleLoginSuccess}
                 onError={() => alert('Google-aanmelding het misluk')}
-                useOneTap
+                ux_mode="popup"
             />
         </div>
     );
