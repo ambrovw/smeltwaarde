674551b340e591bfa846e3cbe73260ebc824594a
Ambro <ambro@ambrovanwyk.com>
2025-10-02 19:50:02 +0200

    Styling

--- src/components/SilverCalculator.jsx
+++ src/components/SilverCalculator.jsx
@@ -177,7 +177,7 @@ function SilverCalculator() {
                                                 {!hideColumns && <th>Fynheid</th>}
                                                 {!hideColumns && <th>Gewig (g)</th>}
                                                 {!hideColumns && <th>Silver (g)</th>}
-                                                {!hideColumns && <th>Elk</th>}
+                                                {!hideColumns && <th className="calc-each">Elk</th>}
                                                 <th>Hoeveelheid</th>
                                                 <th>Waarde (R)</th>
                                             </tr>
@@ -196,7 +196,7 @@ function SilverCalculator() {
                                                         {!hideColumns && <td>{coin.purity}</td>}
                                                         {!hideColumns && <td>{coin.weight}</td>}
                                                         {!hideColumns && <td>{fineSilverGrams.toFixed(2)}</td>}
-                                                        {!hideColumns && <td>R{perItemValue.toFixed(2)}</td>}
+                                                        {!hideColumns && <td className="calc-each">R{perItemValue.toFixed(2)}</td>}
                                                         <td>
                                                             <div className="quantity-control">
                                                                 <button type="button" onClick={() => {
