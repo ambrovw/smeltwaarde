5848085ce416b5ffc6307f830c40421af2e58a43
Ambro <ambro@ambrovanwyk.com>
2025-10-05 00:48:37 +0200

    theme icons

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,34 +1,48 @@
 import { NavLink } from 'react-router-dom';
+import {
+    FaCalculator,     // Waarde Berekening
+    FaCoins,          // Munt Hoeveelhede
+    FaStore,          // Winkel
+    FaBoxOpen,        // Produkte (admin only)
+    FaUser,           // Gebruiker
+    FaSignInAlt,      // Meld aan
+    FaShoppingCart    // Trolley
+} from 'react-icons/fa';
 import '../styles/components/NavHeader.css';
 
 function NavHeader({ role, isLoggedIn }) {
     return (
         <nav className="nav-header">
             <NavLink to="/" className="nav-element" end>
-                Waarde Berekening
+                <FaCalculator title="Waarde Berekening" />
             </NavLink>
 
             <NavLink to="/muntHoeveelhede" className="nav-element">
-                Munt Hoeveelhede
+                <FaCoins title="Munt Hoeveelhede" />
             </NavLink>
 
             <NavLink to="/shop" className="nav-element">
-                Winkel
+                <FaStore title="Winkel" />
             </NavLink>
 
             {role === 'admin' && (
                 <NavLink to="/products" className="nav-element">
-                    Produkte
+                    <FaBoxOpen title="Produkte" />
                 </NavLink>
             )}
 
             {isLoggedIn ? (
-                <NavLink to="/userDetails" className="nav-element">
-                    Gebruiker
-                </NavLink>
+                <>
+                    <NavLink to="/userDetails" className="nav-element">
+                        <FaUser title="Gebruiker" />
+                    </NavLink>
+                    <NavLink to="/cart" className="nav-element">
+                        <FaShoppingCart title="Waentjie" />
+                    </NavLink>
+                </>
             ) : (
                 <NavLink to="/login" className="nav-element">
-                    Meld aan
+                    <FaSignInAlt title="Meld aan" />
                 </NavLink>
             )}
         </nav>
